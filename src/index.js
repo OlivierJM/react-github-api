55da8c3e823c62c1369a22a5a7c0d4bb09d1fa5c
Olivier JM <manolivier93@gmail.com>
2018-11-30 12:22:12 +0200

    querying data form github API

--- src/index.js
+++ src/index.js
@@ -3,23 +3,64 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ApolloClient from "apollo-boost";
+import ApolloClient from "apollo-client";
 import gql from "graphql-tag";
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 
 // https://api.github.com/graphql
+// 43d68515a8b64a2fbfed12421297308ab733c196
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+ 
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
 const client = new ApolloClient({
-  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 });
 
 client
   .query({
     query: gql`
       {
-        rates(currency: "USD") {
-          currency
+        user(login: "alfeyo") {
+            repositoriesContributedTo(last: 5) {
+            totalCount
+            nodes {
+                name
+            }
+            }
+            repositories(last: 10) {
+            totalCount
+            nodes {
+                name
+                url
+                description
+                stargazers(last: 5) {
+                nodes {
+                    name
+                }
+                }
+            }
+            }
         }
-      }
+        }
+
     `
   })
   .then(result => console.log(result));
