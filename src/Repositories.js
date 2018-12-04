4fb7261c35d44aa7254bf6d1641144147715fda6
Olivier JM <manolivier93@gmail.com>
2018-12-04 10:18:31 +0200

    refactored the componennt

--- src/Repositories.js
+++ src/Repositories.js
@@ -1,39 +1,17 @@
-import React, { useState } from "react";
-import { Repos } from "./GitQuery";
-import _ from "lodash";
-
-// function Repositories(){
-//     const [username, setName] = useState('olivierjm')
-//     // const [debounce, setDebounce] = useState('')
-//     const delayedCallback = _.debounce(handleNameChange, 1000)
-
-//     function handleNameChange({target:{ value }}){
-//         setName(value)
-//     }
-//     function onChange(event) {
-//         //This will ensure that the event is not pooled
-//         event.persist()
-//         delayedCallback(event)
-//       }
-//     return(
-//         <div>
-//             <input value={username} type='text' onChange={onChange} />
-//             <Repos login={username}/>
-//         </div>
-//     )
-// }
+import React from "react"
+import { Repos } from "./GitQuery"
+import _ from "lodash"
 
 class Repositories extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: "olivierjm"
+      query: "" // initial query
     }
     this.delayedCallback = _.debounce(this.updateQuery, 1000)
   }
 
-  updateQuery(event) {
-    const { value } = event.target
+  updateQuery = ({target: { value }}) => {
     this.setState({ query: value })
   }
 
@@ -46,7 +24,7 @@ class Repositories extends React.Component {
     const { query } = this.state
     return (
       <div>
-        <input onChange={this.onChange} />
+        <input onChange={this.onChange} placeholder='type a username' type='text' />
         <Repos login={query}/>
       </div>
     )
