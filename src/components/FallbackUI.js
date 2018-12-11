80990a323372a3380a00b4b1f888cf49388d4e33
Olivier JM <manolivier93@gmail.com>
2018-12-11 20:27:41 +0200

    added react suspense

---
+++ src/components/FallbackUI.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function FallBackUI(){
+    return(
+        <div className='row flex-center'><p className='flex-center'>Loading...</p></div>
+    )
+}
+export default FallBackUI
