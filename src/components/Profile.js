ccc88276eef6a63cb6f046f3a969d7f85f482693
Olivier JM <manolivier93@gmail.com>
2018-12-17 15:26:52 +0200

    created a profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,16 @@
 // this file will be a holder for user's profile info as indicated in the issues
 import React, { Fragment } from "react"
 
+// location
+// name
+// organizations
+// location
+
 const Profile = props => (
   <Fragment>
+    <h2>{props.name}</h2>
     <img src={props.image} alt="github avatar" />
   </Fragment>
 )
+
+export default Profile
