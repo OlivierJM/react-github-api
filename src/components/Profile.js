0e6e462be26f27505386accd8cbf003b76c62531
Olivier JM <manolivier93@gmail.com>
2018-12-17 15:22:50 +0200

    removed the repositories contributed to

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,1 +1,8 @@
 // this file will be a holder for user's profile info as indicated in the issues
+import React, { Fragment } from "react"
+
+const Profile = props => (
+  <Fragment>
+    <img src={props.image} alt="github avatar" />
+  </Fragment>
+)
